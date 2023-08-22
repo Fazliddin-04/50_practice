c18a88f1d8f91b7fe5e1994205b0e753210fad1b
fazliddin pardayev <fazliddinpardayev004@gmail.com>
2023-08-22 15:51:04 +0500

    day 1

---
+++ 01_expanding_card/script.js
@@ -0,0 +1,12 @@
+const cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    removeAllClasses();
+    card.classList.add("active");
+  });
+});
+
+function removeAllClasses() {
+  cards.forEach((card) => card.classList.remove("active"));
+}
